959c4168e738af0bb1658122c741427ccd9152ec
Venkata Naga Manikanta Pathakoti <manikantapathakoti08@gmail.com>
2026-08-13 18:55:41 +0530

    Updated the url

--- shopping-cart-ui/src/CartService.js
+++ shopping-cart-ui/src/CartService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_BASE_URL;
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const API_URL = `${BASE_URL}/api/cart`;
 
 const getCart = () => {
     return axios.get(API_URL);
